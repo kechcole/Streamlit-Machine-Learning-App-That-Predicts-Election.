6212a29ceba5711851317620e932c07fc0028326
Collins Kechir <kechircollins@gmail.com>
2025-05-04 02:49:17 +0300

    add streamlit application

---
+++ 1.Predict US Election - ML App Streamlit/app/main.py
@@ -0,0 +1,16 @@
+import pickle
+import pandas as pd
+import streamlit as st
+
+
+
+def main():
+    st.set_page_config(
+    page_title="Election Predictor",
+    page_icon="🗳️",
+    layout="centered",
+    initial_sidebar_state="auto"
+    )
+
+if __name__ == '__main__':
+    main()
