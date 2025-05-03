777abf2c010c4c04c997215916e607481f584eb5
Collins Kechir <kechircollins@gmail.com>
2025-05-04 02:28:03 +0300

    save model and scaler

--- 1.Predict US Election - ML App Streamlit/Model/main.py
+++ 1.Predict US Election - ML App Streamlit/Model/main.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+import pickle
 
 
 
@@ -18,9 +19,13 @@ def create_model(data):
     # Scale predictors
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
+    
+    # Create the dataframe
+    X = pd.DataFrame(X_scaled, columns=X.columns)
+
 
     # split the data 
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
     # Initialize Random Forest Regressor model with specified parameters
@@ -63,6 +68,12 @@ def main():
     data = fetch_clean_data()
     model, scaler = create_model(data)
 
+    # Save the model, 1.Predict US Election - ML App Streamlit\Model
+    with open('1.Predict US Election - ML App Streamlit/Model/model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+    # Save the scaler
+    with open('1.Predict US Election - ML App Streamlit/Model/scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
 
 
 
