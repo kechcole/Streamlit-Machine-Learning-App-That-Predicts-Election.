eaf49bac8a443911f556f1be996e7f6ccda6763a
Collins Kechir <kechircollins@gmail.com>
2025-05-05 12:02:36 +0300

    add side bar with variable's sliders

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import pickle
 import streamlit as st
+import geopandas as gpd
+import folium
+from streamlit_folium import st_folium
 
 
 # load data 
@@ -36,6 +39,30 @@ def add_side_bar():
     return input_dict
 
 
+
+# Plot map 
+def add_counties_map():
+    
+    gdf = gpd.read_file('Data/Contigous_US_County_Election_Results.gpkg', layer='simplified').to_crs(epsg=4326)
+
+    # Create folium map
+    m = folium.Map(location=[gdf.geometry.centroid.y.mean(), gdf.geometry.centroid.x.mean()], zoom_start=4)
+
+    # Add polygons
+    folium.GeoJson(
+        gdf,
+        style_function=lambda x: {
+            'fillColor': '#228B22',
+            'color': 'black',
+            'weight': 0.5,
+            'fillOpacity': 0.4,
+        }
+    ).add_to(m)
+
+    # Show the map in Streamlit
+    st_data = st_folium(m, width=700, height=500)
+
+
 def main():
     st.set_page_config(
     page_title="Election Predictor",
@@ -50,7 +77,11 @@ def main():
                 Voter turnout, Population demographics, Age distribution, Socioeconomic factors. \
                  By combining these variables, the model can provide a statistical estimate of how different factors influence electoral outcomes at the county level, offering valuable insights into voting patterns and potential election results.")
 
-    st.write(add_side_bar())
+    # add_counties_map()
+    # st.write(add_side_bar())
+
+    # Add layout columns to hold the chart on the left and key model metrcs on the right 
+    st.columns([3])
     
 
 
